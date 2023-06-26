4b3c0c1318a144dcb31de95355e96b4419b7d4ba
Joshua Jarvis <joshuajarvis0711@gmail.com>
2023-06-26 15:27:26 -0600

    Added appbar and drawer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,88 @@
 import './App.css';
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
-import { Menu } from '@mui/icons-material';
+import { AppBar, Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { Menu, Inbox, Mail } from '@mui/icons-material';
+import React from 'react';
 
 function App() {
+
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const drawerWidth = 240;
+
   return (
     <div className="App">
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <Menu />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Photos
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <Box className="AppContent" sx={{ width: { sm: `calc(100% - ${drawerWidth}px)` }, ml: { sm: `${drawerWidth}px` }, }} >
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2, display: { xs: 'block', sm: 'none' } }} onClick={() => setMobileOpen(!mobileOpen)}>
+              <Menu />
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              My Portfolio
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      {/* Mobile drawer */}
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={() => setMobileOpen(!mobileOpen)}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        {getDrawerContents()}
+      </Drawer>
+
+      {/* Desktop drawer */}
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': {
+            boxSizing: 'border-box', width: drawerWidth
+          },
+        }}
+      >
+        {getDrawerContents()}
+      </Drawer >
+
+    </div >
+  );
+}
+
+function getDrawerContents(): JSX.Element {
+  return (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <Inbox /> : <Mail />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <Inbox /> : <Mail />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 }
