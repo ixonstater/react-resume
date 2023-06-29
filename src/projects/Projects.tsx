61389fc40542631fce2fa5eeb8e7b9648575c0e6
Joshua Jarvis <joshuajarvis0711@gmail.com>
2023-06-29 10:13:05 -0600

    Started importing media for projects pages.

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,5 +1,18 @@
 import Page from "../components/Page";
 
 export function Projects(): JSX.Element {
-    return (<Page><div>Projects</div></Page>);
+  return (
+    <Page>
+      <div>Projects</div>
+    </Page>
+  );
 }
+
+// Cheapspeech
+// Flower counter -- pics
+// Tower breaker -- pics
+// Tar recognition
+// AES blog post -- ported
+// Dart LC3 vm / assembler
+// Geoquery
+// Labor tracker -- pics
